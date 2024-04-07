3d3182d8ae6a8bd3751ebd37f5c65664b8f5d063
zijiale <2353520016@qq.com>
2024-04-07 16:11:25 +0800

    feat: i know there is not have something

---
+++ aop-jdk-proxy/src/main/java/com/jiale/aopjdkproxy/AopJdkProxyApplication.java
@@ -0,0 +1,13 @@
+package com.jiale.aopjdkproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopJdkProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopJdkProxyApplication.class, args);
+    }
+
+}
---
+++ aop-jdk-proxy/src/test/java/com/jiale/aopjdkproxy/AopJdkProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiale.aopjdkproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopJdkProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
