515d4bbd2c7bd9d492a7ee68a110f9796d39a275
zijiale <2353520016@qq.com>
2024-10-21 16:56:41 +0800

    feat: jdk proxy and preview spring security

---
+++ vue/vue-project/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <header>
+
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+}
+</style>
